2585e6c31abf746ebaf76954e6007c19ea036c2f
Luisa Negri <luisaanegri@gmail.com>
2023-09-20 18:51:16 +0200

    feat: Implement HomeScreen component with FlatList for city names and handle loading/error states

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,51 @@
-import { View, Button, Text } from "react-native";
-import { Routes } from "../navigation/routes";
+import { FlatList, Text, View, StyleSheet, Button } from 'react-native';
+import { Routes } from '../navigation/routes';
+import useWeather from '../api/useWeather';
+
 
 const HomeScreen = ({ navigation }) => {
+    const { loading, error, weatherData } = useWeather();
+
+    if (loading) {
+        return <Text style={styles.infoText}>Loading...</Text>;
+    }
+
+    if (error) {
+        return <Text style={styles.errorText}>Error fetching weather data. Please try again.</Text>;
+    }
+
+    const cityNames = weatherData ? Object.keys(weatherData).sort() : [];
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home Screen</Text>
-            <Button
-                title="Go to Weather Details"
-                onPress={() => navigation.navigate(Routes.WeatherDetails)}
+        <View style={styles.container}>
+            <FlatList
+                data={cityNames}
+                keyExtractor={(city) => city}
+                renderItem={({ item: cityName }) => (
+                    <Button
+                        title={cityName}
+                        onPress={() => navigation.navigate(Routes.WeatherDetails)}
+                    />
+                )}
             />
         </View>
     );
-}
+};
 
-export default HomeScreen
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    infoText: {
+        padding: 15,
+        textAlign: 'center',
+    },
+    errorText: {
+        padding: 15,
+        color: 'red',
+        textAlign: 'center',
+    },
+});
+
+export default HomeScreen;
+
