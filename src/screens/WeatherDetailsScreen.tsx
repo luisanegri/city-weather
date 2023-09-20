73c7dd8c54f8991fe9b67074124ba9cc765e8a3f
Luisa Negri <luisaanegri@gmail.com>
2023-09-20 21:38:06 +0200

    feat: add WeatherDetailsScreen with temperature conversion and chronological sorted display

--- src/screens/WeatherDetailsScreen.tsx
+++ src/screens/WeatherDetailsScreen.tsx
@@ -1,10 +1,44 @@
-import { Text, View } from "react-native"
+import CityWeatherList from '../components/CityWeatherList';
+import CityImageWithLabel from '../components/CityImageWithLabel';
+import { CityWeatherData } from "../types/weatherTypes"
 
-const WeatherDetailsScreen = () => {
+const WeatherDetailsScreen = ({ route }) => {
+    const weatherDataForCity: CityWeatherData = route.params.weatherDataForCity
+
+    const convertToCelsius = (temp: number, tempType: string) => {
+        switch (tempType) {
+            case "F":
+                return (temp - 32) / 1.8; // Fahrenheit to Celsius
+            case "K":
+                return temp - 273.15; // Kelvin to Celsius
+            case "C":
+            default:
+                return temp; // Already in Celsius
+        }
+    };
+
+    const sortedAndConvertedWeather = weatherDataForCity.weather
+        .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+        .map(item => ({
+            ...item,
+            temp: Math.round(convertToCelsius(item.temp, item.tempType)),
+            tempType: "C"
+        }));
+
+    const sortedWeatherDataForCity = {
+        ...weatherDataForCity,
+        weather: sortedAndConvertedWeather
+    };
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Weather Details Screen</Text>
-        </View>
+        <>
+            <CityImageWithLabel
+                cityName={weatherDataForCity.city.name}
+                imageUrl={weatherDataForCity.city.picture}
+            />
+            <CityWeatherList weatherData={sortedWeatherDataForCity.weather} />
+        </>
+
     )
 }
 
