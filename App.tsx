6f598e7f65b39388899992cd855ad5c5df9c382b
Luisa Negri <luisaanegri@gmail.com>
2023-09-22 17:44:45 +0200

    feat: setup React Query with persistent caching to ensure data is available even when offline or after app restarts.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,31 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query';
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 
 import AppNavigator from './src/navigation/AppNavigator';
+
+const persister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+  throttleTime: 3000,
+});
 
 export default function App() {
 
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      onSuccess={() =>
+        // Fresh data on app initialization
+        queryClient.invalidateQueries()
+      }
+      persistOptions={{ persister }}
+      client={queryClient}>
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   );
 }
