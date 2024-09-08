cf7594b438c5ce5378e24aba06d283ac84af4682
Luisa Negri <luisaanegri@gmail.com>
2024-09-08 09:42:27 -0300

    feat: optimize caching and retry logic in useWeather hook
    - Set staleTime and cacheTime to 1 hour for better cache management.
    - Enabled keepPreviousData to maintain data during refetch for improved user experience.
    - Added custom retry logic to prevent retries on server errors (500) and limit retries to 3 for other errors.

--- src/api/useWeather.ts
+++ src/api/useWeather.ts
@@ -1,37 +1,58 @@
-import { useQuery } from '@tanstack/react-query';
-import { AxiosError } from 'axios';
-import { customAxios } from '../http/interceptors';
+import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { customAxios, CustomAxiosError } from "../http/interceptors";
 
-import { CityWeatherData } from '../types/weatherTypes';
-import { WEATHER_DATA_KEY } from '../utils/constants';
-import { mergeWeatherDataByCity } from '../utils/weatherData';
+import { CityWeatherData, MergedWeatherData } from "../types/weatherTypes";
+import { WEATHER_DATA_KEY } from "../utils/constants";
+import { mergeWeatherDataByCity } from "../utils/weatherData";
 
-const fetchWeatherData = async () => {
+const fetchWeatherData = async (): Promise<MergedWeatherData> => {
   try {
-    const response = await customAxios.get('weather');
+    const response = await customAxios.get("weather");
+
     return mergeWeatherDataByCity(response.data);
   } catch (error) {
-    throw error;
+    throw error; // Axios interceptors will handle the error
   }
 };
 
 const useWeather = () => {
-  const { data: weatherData, isLoading, isError, error, refetch } = useQuery({
+  const {
+    data: weatherData,
+    isLoading,
+    isError,
+    error,
+    refetch,
+  } = useQuery({
     queryKey: [WEATHER_DATA_KEY],
     queryFn: fetchWeatherData,
-    // Ensures that once data is fetched and cached, it remains available and "fresh" 
-    // for the lifetime of the application, optimizing for offline access and minimizing network requests.
-    staleTime: Infinity,
-    cacheTime: Infinity,
+    staleTime: 60 * 60 * 1000,
+    cacheTime: 60 * 60 * 1000,
+    keepPreviousData: true, // Keeps previous data during refetching to improve user experience
+    retry: (failureCount, error) => {
+      const axiosError = error as AxiosError;
+      if (axiosError.response?.status === 500) {
+        return false;
+      }
+      return failureCount < 3;
+    },
     onSuccess: (data) => {
       console.log("Query was successful! Data:", data);
     },
+    onError: (error) => {
+      console.log("Query failed! Error:", error);
+    },
   });
 
-  const errorMessage = isError && error ? (error as AxiosError).message : null;
+  const errorMessage =
+    isError && error instanceof AxiosError
+      ? (error as CustomAxiosError).customMessage || error.message
+      : null;
 
-  const getWeatherDetailsForCity = (cityName: string): CityWeatherData | null => {
-    return weatherData ? weatherData[cityName] : null;
+  const getWeatherDetailsForCity = (
+    cityName: string
+  ): CityWeatherData | null => {
+    return weatherData?.[cityName] ?? null;
   };
 
   return {
